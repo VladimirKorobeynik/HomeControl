aabf17f06339af667a2847b4d4e5805b41f0ed07
Владимир Коробейник <vladimirkorobeinyk@MacBook-Pro-Vladimir.local>
2021-03-23 18:07:05 +0200

    Delete some files

--- loader.js
+++
@@ -1,9 +0,0 @@
-//Прелоадер
-window.onload = function() {
-    const preloader = document.getElementById('preloader');
-    setTimeout(function() {
-        if (!preloader.classList.contains('pageLoad')) {
-            preloader.classList.add('pageLoad');
-        }
-    }, 500);
-}
